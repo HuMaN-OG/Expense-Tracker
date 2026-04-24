891f526edb3887cbd21858827c47ab2f0a572d71
HuMaN-OG <HuMaN-OG@users.noreply.github.com>
2026-04-24 10:07:49 +0000

    deploy: ba5ac1085d88c699790a7861227a1950b6df74a8

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     // When deployed, it will use your production URL.
     API_URL: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
         ? 'http://localhost:5000/api'
-        : 'https://REPLACE_WITH_YOUR_DEPLOYED_BACKEND_URL.com/api' 
+        : 'https://expense-tracker-me8g.onrender.com/api'
 };
 
 window.CONFIG = CONFIG;
