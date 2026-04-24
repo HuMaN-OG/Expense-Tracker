a90478860294d4ea875a6c79fa9dc054b0e23b75
HuMaN-OG <HuMaN-OG@users.noreply.github.com>
2026-04-24 10:37:22 +0000

    deploy: f7f8c043c9b04ffd66b51e39a37563de9339a32a

--- js/budget.js
+++ js/budget.js
@@ -1,4 +1,4 @@
-const BASE_URL = CONFIG.API_URL;
+const BASE_URL = (typeof CONFIG !== 'undefined') ? CONFIG.API_URL : 'http://localhost:5000/api';
 
 document.addEventListener('DOMContentLoaded', () => {
   const token = localStorage.getItem('token');
